edd87665cc7e6dfd96a5166cd6111a7d47323c32
Cliff McCall <cliff.mccall1@gmail.com>
2018-05-20 09:57:48 -0500

    Bethel Tech project for personal web page

---
+++ script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){    
+    $('img').hover(function(){
+        $(this).css("max-height", "65%");
+    }, function(){
+        $(this).css("max-height", "55%");
+    });
+});
+
+// $(document).ready(function () {
+//     loadRepo();
+// });
+function loadRepo(){
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && 200){
+
+            results = JSON.parse(this.responseText);
+            for(var i=0; i < results.length; i++){
+                console.log(results[i].name);
+
+                var ul = document.getElementById("repositories");
+                var li = document.createElement("li");
+                var a = document.createElement("a");
+
+                li.appendChild(document.createTextNode(results[i].name));
+
+                a.appendChild(li);
+                a.setAttribute("href",results[i].html_url);
+                ul.appendChild(a);
+            }
+        }
+    }
+    xhttp.open("GET", "https://api.github.com/users/cliffmccall1/repos", true);
+    xhttp.send();
+}
